b4db82c50d3349f0c7db36160a7243d96ce34e27
zkf101 <42493830+zkf101@users.noreply.github.com>
2018-08-19 01:08:57 +0900

    Update TenmaGabriel.js

--- TenmaGabriel.js
+++ TenmaGabriel.js
@@ -60,10 +60,10 @@ TenmaGabriel.on('message', message => {
 })
   
   TenmaGabriel.on('message', message => {
-    if (message.content == 'g원본이미지') {
-        message.replay("http://blogfiles.naver.net/MjAxNzAyMTRfMjgw/MDAxNDg3MDc5ODI1NTQ1.6MLcaOqY5dQIZAoxVTl4rCE2iUCROpD3VmMWULKZnQEg.yenjhVZmOOm_f2rO2m1pFhrmgoy60NWqx-3y_PeoH2Yg.PNG.sooyong1492/%B0%A1%BA%EA%B8%B1_%B5%E5%B7%D3%BE%C6%BF%F4_06.mp4_001201434.png");
+    if (message.content === 'grip') {
+        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        message.channel.send(attachment);
     }
-})
-
+});
 
 TenmaGabriel.login(process.env.BOT_TOKEN);
