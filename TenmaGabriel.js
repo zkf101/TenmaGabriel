45c4fdba847a21f20c5947c26ed13e2622409074
zkf101 <42493830+zkf101@users.noreply.github.com>
2018-08-18 23:30:16 +0900

    Update TenmaGabriel.js

--- TenmaGabriel.js
+++ TenmaGabriel.js
@@ -1,34 +1,24 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const TenmaGabriel = new Discord.Client();
-const google = require("google");
-const YTDL = require("ytdl-core");
-const { Client, Attachment } = require('discord.js');
-const fs = require('fs');
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
 
-enmaGabriel.on("message", (message)=> {
+
+TenmaGabriel.on("message", (message)=> {
     if(message.content == "가브릴"){
         message.reply("왜?");
     }
 })
 
 
+TenmaGabriel.on('ready', () => {
+    console.log('I am ready!');
+});
 
-TenmaGabriel.on('message', message => {
-    if(!message.guild) return;
-    if (message.content === '/가브릴'){
-        if (message.member.voiceChannel){
-            message.member.voiceChannel.join()
-            .then(connection => {
-                message.reply('나왔다!!');
-            })
-            .catch(console.log);
-        } else {
-            message.reply('먼저방에들어가');
-        }
+TenmaGabriel.on("message", message => {
+    if(message.content === 'ping'){
+        message.reply('pong');
     }
 });
+
 
 TenmaGabriel.on('message', message => {
     if(message.content == '/우마루'){
@@ -69,50 +59,5 @@ TenmaGabriel.on('message', message => {
     }
 })
 
-
-TenmaGabriel.on('ready', () => {
-    console.log('I am ready!');
-  });
-  
-  TenmaGabriel.on('message', message => {
-    if (message.content === 'grip') {
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        message.channel.send(attachment);
-    }
-});
-
-TenmaGabriel.on('ready', () => {
-    console.log('I am ready!');
-  });
-  
-  TenmaGabriel.on('message', message => {
-    if (message.content === 'g원본이미지') {
-        const attachment = new Attachment('http://blogfiles.naver.net/MjAxNzAyMTRfMjgw/MDAxNDg3MDc5ODI1NTQ1.6MLcaOqY5dQIZAoxVTl4rCE2iUCROpD3VmMWULKZnQEg.yenjhVZmOOm_f2rO2m1pFhrmgoy60NWqx-3y_PeoH2Yg.PNG.sooyong1492/%B0%A1%BA%EA%B8%B1_%B5%E5%B7%D3%BE%C6%BF%F4_06.mp4_001201434.png');
-        message.channel.send(attachment);
-    }
-});
-
-
-TenmaGabriel.on('message', message => {
-    if (message.content === 'ㅂㅇㄹ') {
-        const attachment = new Attachment('https://cdn.discordapp.com/attachments/378117883160428544/479627514046709761/received_1952074638448418.jpeg');
-        message.channel.send(attachment);
-    }
-});
-
-
-TenmaGabriel.on('message', message => {
-    if (message.content === 'ㅁㄸ') {
-        const attachment = new Attachment('https://cdn.discordapp.com/attachments/434763264165937183/479628059264417807/received_1954560718199810.jpeg');
-        message.channel.send(attachment);
-    }
-});
-
-TenmaGabriel.on('message', message => {
-    if (message.content === '미띤') {
-        const attachment = new Attachment('https://cdn.discordapp.com/attachments/434763264165937183/479628059264417807/received_1954560718199810.jpeg');
-        message.channel.send(attachment);
-    }
-});
 
 TenmaGabriel.login(process.env.BOT_TOKEN);
